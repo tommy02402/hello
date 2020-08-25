9ac115313c87aedb22ce4c3a547a396d5e060bdd
tommy02402 <69862474+tommy02402@users.noreply.github.com>
2020-08-25 12:54:54 -0500

    Add files via upload

---
+++ hello.c.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Tommy Nguyen
+ *         tnguyen155@huskers.unl.edu
+ * Date: 2020/08/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Tommy Nguyen!\n");
+
+  return 0;
+}
